4860d29ecaec1113d57bb73a0cbe73f443627558
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2022-05-10 14:05:49 -0300

    build: Pagination Layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,11 +6,13 @@ import {
 import Listing from 'pages/Listing';
 import Form from 'pages/Form';
 import NavBar from "components/NavBar";
+import Pagination from "components/Pagination";
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
+      <Pagination />
       <Routes>
         <Route path="/" element={<Listing />} />
         <Route path="/form">
