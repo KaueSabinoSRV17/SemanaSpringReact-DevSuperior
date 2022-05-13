267eeab2412df262409beb71d188aced73a4b13a
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2022-05-13 10:59:29 -0300

    buid: Homologado novamente no Postgres (antes em MySQL)

--- backend/src/main/java/com/SemanaSpringReact/dsmovie/controller/ScoreController.java
+++ backend/src/main/java/com/SemanaSpringReact/dsmovie/controller/ScoreController.java
@@ -1,6 +1,7 @@
 package com.SemanaSpringReact.dsmovie.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ public class ScoreController {
 	private ScoreService ss;
 	
 	@PutMapping
+	@Transactional(readOnly = false)
 	public MovieDTO SaveScroe(@RequestBody ScoreDTO dto) {
 		MovieDTO movieDTO = ss.saveScore(dto);
 		return movieDTO;
